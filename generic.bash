02b0e1cf83a4abea6d171597dd2944b4e71d5251
Alexander, Michael <michael.alexander@karlstorz.com>
2019-04-23 10:46:51 -0700

    Convert to support both zsh and bash

---
+++ generic.bash
@@ -0,0 +1,34 @@
+# Basic generic settings.  Includes history size and settings,
+# aliases for colorization, git status in the prompt, and user based process aliasing.
+#
+# Requires git 
+# No arguments
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# add the git settings to the command prompt
+if [ "$color_prompt" = yes ]; then
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;34m\]\w\[\033[00m\]\[\033[1;33m\]$(__git_ps1)\[\033[00m\] \$ '
+else
+    PS1='${debian_chroot:+($debian_chroot)}\w$(__git_ps1) \$ '
+fi
+
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto '
+    #alias dir='dir --color=auto'
+    #alias vdir='vdir --color=auto'
+
+    alias grep='grep --color=auto '
+    alias fgrep='fgrep --color=auto '
+    alias egrep='egrep --color=auto '
+fi
+
+#force parsing of the correct .Xdefaults file since Ubuntu apparently
+#dislikes doing it
+xrdb ~/.Xdefaults
+
+
+
