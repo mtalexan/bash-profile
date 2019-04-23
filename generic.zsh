c86fc160e19dda8a506f6ace84e906752596b0cb
Alexander, Michael <michael.alexander@karlstorz.com>
2019-04-23 16:49:42 -0700

    Better zsh options

--- generic.zsh
+++ generic.zsh
@@ -1,4 +1,4 @@
 HISTSIZE=100000
 SAVEHIST=100000
-setopt appendhistory extendedglob nomatch notify complete_aliases nolistambiguous
+setopt appendhistory extendedglob nomatch notify complete_aliases listambiguous pushdignoredups noautomenu nomenucomplete histignoredups histignorealldups histsavenodups histverify sharehistory noflowcontrol
 unsetopt autocd
