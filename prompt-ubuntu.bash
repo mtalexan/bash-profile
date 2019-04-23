4bf021a91d64beabe800bd8a53d5f23ac1dad60e
Alexander, Michael <michael.alexander@karlstorz.com>
2019-04-23 15:55:49 -0700

    Add bash support for powerline-go

--- prompt-ubuntu.bash
+++ prompt-ubuntu.bash
@@ -1,6 +1,10 @@
-# add the git settings to the command prompt
-if [ "$color_prompt" = yes ]; then
-    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;34m\]\w\[\033[00m\]\[\033[1;33m\]$(__git_ps1)\[\033[00m\] \$ '
+if [ -n "$USE_POWERLINE" ] && [ $USE_POWERLINE -eq 1 ] ; then
+    source ${LOCAL_PROFILES_DIR}/powerline.bash
 else
-    PS1='${debian_chroot:+($debian_chroot)}\w$(__git_ps1) \$ '
+    # add the git settings to the command prompt
+    if [ "$color_prompt" = yes ]; then
+        PS1='${debian_chroot:+($debian_chroot)}\[\033[01;34m\]\w\[\033[00m\]\[\033[1;33m\]$(__git_ps1)\[\033[00m\] \$ '
+    else
+        PS1='${debian_chroot:+($debian_chroot)}\w$(__git_ps1) \$ '
+    fi
 fi
