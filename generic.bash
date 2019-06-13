81fd5139abf9c611cfe00ebcf37c2f40d42e5615
Alexander, Michael <michael.alexander@karlstorz.com>
2019-06-13 09:43:13 -0700

    fixed the ubuntu detection

--- generic.bash
+++ generic.bash
@@ -15,7 +15,7 @@ HISTFILESIZE=200000
 # append to the history file, don't overwrite it
 shopt -s histappend
 
-if [[ "$(uname -n)" = "ubuntu" ]] ; then
+if uname -a | grep -iq "ubuntu" ; then
     source ${LOCAL_PROFILES_DIR}/prompt-ubuntu.bash
 else
     source ${LOCAL_PROFILES_DIR}/prompt-manjaro.bash
@@ -33,7 +33,7 @@ if [ -x /usr/bin/dircolors ]; then
     alias egrep='egrep --color=auto '
 fi
 
-if [[ "$(uname -n)" = "ubuntu" ]] ; then
+if uname -a | grep -iq "ubuntu" ; then
     #force parsing of the correct .Xdefaults file since Ubuntu apparently
     #dislikes doing it
     xrdb ~/.Xdefaults
