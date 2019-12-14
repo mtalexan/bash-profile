fe4463f1b841102666994c275ce7af654328e8b0
Mike Alexander <mikealexander1860@gmail.com>
2019-12-13 21:57:01 -0800

    Add fzf-fasd and up plugins as submodules and sourced them

--- generic.zsh
+++ generic.zsh
@@ -4,7 +4,8 @@ setopt appendhistory extendedglob nomatch notify complete_aliases listambiguous 
 unsetopt autocd
 
 # zsh-bd
-. ${LOCAL_PROFILES_DIR}/tools/zsh-bd/bd.zsh
+source ${LOCAL_PROFILES_DIR}/tools/zsh-bd/bd.zsh
+
 # peco history
 # Requires manual installation of the peco executable from https://github.com/peco/peco/releases
 export PATH=${LOCAL_PROFILES_DIR}/tools/peco_linux_amd64:${PATH}
@@ -12,4 +13,10 @@ export PATH=${LOCAL_PROFILES_DIR}/tools/peco_linux_amd64:${PATH}
 export ZSH_PECO_HISTORY_OPTS="--layout=bottom-up --initial-filter=SmartCase --select-1"
 # set to non-zero to dedup history before sending to peco
 export ZSH_PECO_HISTORY_DEDUP=1
-. ${LOCAL_PROFILES_DIR}/tools/zsh-peco-history/zsh-peco-history.zsh
+source ${LOCAL_PROFILES_DIR}/tools/zsh-peco-history/zsh-peco-history.zsh
+
+#fzf-fasd, allows selection of tab completion of the "z" command
+source ${LOCAL_PROFILES_DIR}/tools/fzf-fasd/fzf-fasd.plugin.zsh
+
+#up command
+source ${LOCAL_PROFILES_DIR}/tools/up.zsh/up.plugin.zsh
