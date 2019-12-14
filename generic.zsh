328ffddfbd3d1911a969116af93437dc3c2f8f13
Mike Alexander <mikealexander1860@gmail.com>
2019-12-13 18:49:59 -0800

    Add zsh-bd submodule and tool

--- generic.zsh
+++ generic.zsh
@@ -2,3 +2,6 @@ HISTSIZE=100000
 SAVEHIST=100000
 setopt appendhistory extendedglob nomatch notify complete_aliases listambiguous pushdignoredups noautomenu nomenucomplete histignoredups histignorealldups histsavenodups histverify sharehistory noflowcontrol
 unsetopt autocd
+
+# zsh-bd
+. ${LOCAL_PROFILES_DIR}/tools/zsh-bd/bd.zsh
